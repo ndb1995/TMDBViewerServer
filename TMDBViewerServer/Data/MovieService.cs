a94506413a660c68c903a9864a0d8dce87e64b45
ndb1995 <ndbielinski@gmail.com>
2022-01-16 18:16:12 -0500

    Add project files.

---
+++ TMDBViewerServer/Data/MovieService.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using TMDbLib.Client;
+using TMDbLib.Objects.Movies;
+
+namespace TMDBViewerServer.Data
+{
+    public class MovieService
+    {
+
+        TMDbClient client = new TMDbClient("8bd5a4c221de15578361b66eab285db8");
+
+        /// <summary>
+        /// get a movie based off of tmdb movie id
+        /// </summary>
+        public async Task<Movie> getMovieTMDB(int id)
+        {
+            Movie movie = await client.GetMovieAsync(id, MovieMethods.Credits | MovieMethods.Videos);
+
+            return movie;
+        }
+
+        /// <summary>
+        /// get a movie based off of IMDB movie id
+        /// </summary>
+        public async Task<Movie> getMovieIMDB(string name)
+        {
+            Movie movie = await client.GetMovieAsync(name, MovieMethods.Credits | MovieMethods.Videos);
+
+            return movie;
+        }
+    }
+}
